7b7e74b1cd37af5d11d6d0b5f937fc55ff230e50
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2026-05-08 20:36:53 -0300

    Uso del LoginViewModel  y manejo de mensajes
    Se delega la validación y el inicio de sesión a vm.recuperarDatos() y observa getMsj() para mostrar notificaciones mediante Toast.

--- app/src/main/java/com/blonder/inmobiliaria/LoginActivity.java
+++ app/src/main/java/com/blonder/inmobiliaria/LoginActivity.java
@@ -3,30 +3,40 @@ package com.blonder.inmobiliaria;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.blonder.inmobiliaria.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
     private ActivityLoginBinding binding;
+    private LoginViewModel vm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        vm = ViewModelProvider.AndroidViewModelFactory.getInstance(this.getApplication()).create(LoginViewModel.class);
+
         binding.btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+                String email = binding.etEmail.getText().toString();
+                String pass = binding.etPass.getText().toString();
+                vm.recuperarDatos(email, pass);
+
+
             }
+        });
+        vm.getMsj().observe(this, msj -> {
+            Toast.makeText(LoginActivity.this, msj, Toast.LENGTH_SHORT).show();
         });
 
     }
