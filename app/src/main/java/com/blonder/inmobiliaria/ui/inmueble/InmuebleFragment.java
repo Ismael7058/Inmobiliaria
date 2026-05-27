33abfd3e3eeb6aaf10fe9bca56051a8856507859
Máximo quiroga <123843504+MaxIIIDev@users.noreply.github.com>
2026-05-27 13:14:07 -0300

    Update InmuebleFragment.java
    Faltaba agregarle el valor booleano de validacion a la funcion establecida.

--- app/src/main/java/com/blonder/inmobiliaria/ui/inmueble/InmuebleFragment.java
+++ app/src/main/java/com/blonder/inmobiliaria/ui/inmueble/InmuebleFragment.java
@@ -104,7 +104,7 @@ public class InmuebleFragment extends Fragment {
         /// Listener del Boton Guardar
         b.btnGuardarInmueble.setOnClickListener(v -> {
             setEnabledSupreme(false,false);
-            setVisibleSupreme(1);
+            setVisibleSupreme(1, true);
             Toast.makeText(getContext(), "Lógica para guardar cambios", Toast.LENGTH_SHORT).show();
         });
         /// Listener del CheckBox para cambiar la disponibilidad
