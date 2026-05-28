bcd875950965d0dcb6ce227ce68f49257a2e66ae
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2026-05-28 16:26:57 -0300

    Se implementó el diálogo de confirmación para cerrar sesión en LogoutFragment

--- app/src/main/java/com/blonder/inmobiliaria/ui/logout/LogoutFragment.java
+++ app/src/main/java/com/blonder/inmobiliaria/ui/logout/LogoutFragment.java
@@ -1,18 +1,23 @@
 package com.blonder.inmobiliaria.ui.logout;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.blonder.inmobiliaria.LoginActivity;
 import com.blonder.inmobiliaria.R;
+import com.blonder.inmobiliaria.request.ApiClient;
 
 public class LogoutFragment extends Fragment {
 
@@ -23,16 +28,22 @@ public class LogoutFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mostrarDialogoLogout();
         return inflater.inflate(R.layout.fragment_logout, container, false);
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(LogoutViewModel.class);
-        // TODO: Use the ViewModel
+    private void mostrarDialogoLogout() {
+        AlertDialog show = new AlertDialog.Builder(requireContext()).setTitle("Cerrar Sesión").setMessage("¿Está seguro que desea salir?").setCancelable(false).setPositiveButton("Aceptar", (dialogInterface, i) -> {
+            ///Borro el token
+            ApiClient.eliminarToken(requireContext());
+            Intent intent = new Intent(requireContext(), LoginActivity.class);
+            startActivity(intent);
+            requireActivity().finish();
+        }).setNegativeButton("Cancelar", (dialogInterface, i) -> {
+            /// Se queda en home
+            Navigation.findNavController(requireActivity(), R.id.nav_host_fragment_content_main).navigate(R.id.nav_home);
+        }).show();
     }
 
 }
