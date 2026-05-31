95e3f186a4d885058ae7227577b1f96e63c70240
Ariel Ismael Miranda Salmin <estiloisma70@gmail.com>
2026-05-31 18:01:41 -0300

    [feat]: implementación de funcionalidad de llamada a partir de sacudir el dispositivo

--- app/src/main/java/com/blonder/inmobiliaria/ui/home/HomeFragment.java
+++ app/src/main/java/com/blonder/inmobiliaria/ui/home/HomeFragment.java
@@ -1,15 +1,26 @@
 package com.blonder.inmobiliaria.ui.home;
 
+import static androidx.core.content.ContextCompat.registerReceiver;
+
 import androidx.activity.OnBackPressedCallback;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.CallLog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,7 +42,6 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         fragmentHomeBinding = FragmentHomeBinding.inflate(inflater, container, false);
         mViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
-
         mViewModel.getMapaActual().observe(getViewLifecycleOwner(), new Observer<HomeViewModel.MapaActual>() {
             @Override
             public void onChanged(HomeViewModel.MapaActual mapaActual) {
@@ -51,6 +61,22 @@ public class HomeFragment extends Fragment {
         };
         // Agregar el callback al sistema
         requireActivity().getOnBackPressedDispatcher().addCallback(getViewLifecycleOwner(), callback);
+
+        mViewModel.getAbrirTelefono().observe(getViewLifecycleOwner(), new Observer<Intent>() {
+            @Override
+            public void onChanged(Intent intent) {
+                startActivity(intent);
+            }
+        });
+
+        // Escuchar el evento de sacudida
+        mViewModel.comenzarDeteccion();
+
         return fragmentHomeBinding.getRoot();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
     }
 }
